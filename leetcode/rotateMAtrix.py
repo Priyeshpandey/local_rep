b047456bd292099b3a2434b7e9be2da9b35e6bd2
priyepan <priyesh.pandey@oracle.com>
2021-07-26 09:34:57 +0530

    rotateMAtrix.py done

--- leetcode/rotateMAtrix.py
+++ leetcode/rotateMAtrix.py
@@ -1,17 +1,16 @@
-# todo
+# Done
 def rotate(matrix):
     n = len(matrix)
 
     for i in range(n//2):
-        for j in range(n-1):
-            matrix[i][j], matrix[j][n-j-1], \
-            matrix[n-i-1][n-j-1], matrix[n-j-1][j] = \
-                matrix[n - j - 1][j], matrix[i][j], matrix[j][n - j - 1], \
-                matrix[n - i - 1][n - j - 1]
+        for j in range(i,n-i-1):
+            matrix[i][j], matrix[j][n-i-1], \
+            matrix[n-i-1][n-j-1], matrix[n-j-1][i] = matrix[n-j-1][i], matrix[i][j],\
+                                                     matrix[j][n-i-1],matrix[n-i-1][n-j-1]
 
 
 if __name__=='__main__':
-    matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    matrix = [[1,2,3,4],[4,5,6,7],[7,8,9,10], [3,5,1,12]]
     for row in matrix:
         print(*row, sep='\t')
 
